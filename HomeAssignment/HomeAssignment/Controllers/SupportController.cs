03af0f0c871e5aaa7b2a4ad2d2ba0c4cd3dd72db
LeanderPace <leander.pace.b42329@mcast.edu.mt>
2020-12-21 16:34:28 +0100

    Added the contact us page with some minor validation, created the app, data, domain, IOC folder and installed the necessary NUGET Packages

---
+++ HomeAssignment/HomeAssignment/Controllers/SupportController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeAssignment.Controllers
+{
+    public class SupportController : Controller
+    {
+        [HttpGet] 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(string email, string message) // parameters need to match the name in input/textarea in Contact.cshtml
+        {
+            if(string.IsNullOrEmpty(email)) { 
+                ViewData["danger"] = "Email was left empty";
+            }
+            else if(string.IsNullOrEmpty(message)) {
+                ViewData["danger"] = "Message was left empty";
+            } 
+            else
+            {
+                ViewData["feedback"] = "Thank you for your query, we will get back to your shortly";
+            }
+            return View();
+        }
+    }
+}
